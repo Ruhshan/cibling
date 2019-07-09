19aabde0d7186bb76a84e665203512dd472fb2ba
Ruhshan <ruhshan.ahmed@gmail.com>
2019-07-10 01:00:56 +0600

    rebuilding posts and comments

--- cibling_web/serializers.py
+++ cibling_web/serializers.py
@@ -1,8 +1,20 @@
-from .models import Post
-from rest_framework.serializers import ModelSerializer
+from .models import Post, Comment
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
 
 
 class PostSerializer(ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+
     class Meta:
         model = Post
         fields = "__all__"
+
+
+
--- cibling_web/static/custom/js/newsfeed.js
+++ cibling_web/static/custom/js/newsfeed.js
@@ -1,5 +1,28 @@
-$(document).ready(function (){
-    $("#all-posts").paginate({
-        perPgae: 15
-    });
+// $(document).ready(function (){
+//     $("#all-posts").paginate({
+//         perPgae: 15
+//     });
+// });
+
+var app = new Vue({
+   el:"#all-posts",
+   data:{
+      posts:[]
+   } ,
+   created(){
+       console.log("created")
+       this.get_posts()
+   },
+
+   methods:{
+     get_posts:function(){
+         var self = this
+
+         axios.get("/api/cibling-web/posts").then((respose)=>{
+             self.posts = respose.data
+         }).catch((err)=>{
+             console.log(err)
+         })
+     }
+   }
 });
