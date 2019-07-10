d3d4c04c60500e1b40c9fd16c8618b5b817715d9
Ruhshan <ruhshan.ahmed@gmail.com>
2019-07-10 22:33:54 +0600

    enabled scrollability

--- cibling_web/apis.py
+++ cibling_web/apis.py
@@ -1,20 +1,26 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+
 from .serializers import PostSerializer
 from .models import Post
-from django.db.models import Q
+import time
 
-
+class PostResultPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 class ListPosts(ListAPIView):
     serializer_class = PostSerializer
+    pagination_class = PostResultPagination
 
     def get_queryset(self):
+        time.sleep(3)
         author_ids = list(self.request.user.cibling_1.values_list("cibling_2__id", flat=True))
         author_ids.extend(list(self.request.user.cibling_2.values_list("cibling_1__id", flat=True)))
         author_ids.append(self.request.user.id)
         queryset = Post.objects.filter(author__in=author_ids).order_by('-time_posted')
 
         return queryset
-
 
 
--- cibling_web/static/custom/js/newsfeed.js
+++ cibling_web/static/custom/js/newsfeed.js
@@ -5,66 +5,82 @@
 // });
 
 var app = new Vue({
-   el:"#all-posts",
-   data:{
-      posts:[],
-      user:""
-   } ,
-   created(){
-       console.log("created")
-       this.get_myinfo()
-       this.get_posts()
-   },
+    el: "#all-posts",
+    data: {
+        page: 1,
+        posts: [],
+        user: ""
+    },
+    created() {
+        console.log("created")
+        this.get_myinfo()
+        //this.get_posts()
+    },
 
-   filters:{
-     formatDate:function (date) {
-         return moment(date).format('MMMM Do YYYY, h:mm:ss a')
-     }
-   },
+    filters: {
+        formatDate: function (date) {
+            return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+        }
+    },
 
-   methods:{
-     get_posts:function(){
-         var self = this
+    methods: {
+        get_posts: function () {
+            var self = this
 
-         axios.get("/api/cibling-web/posts").then((respose)=>{
-             self.posts = respose.data
-         }).catch((err)=>{
-             console.log(err)
-         })
-     },
-     get_myinfo:function () {
-         var self = this
+            axios.get("/api/cibling-web/posts").then((respose) => {
+                self.posts = respose.data.results
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+        get_myinfo: function () {
+            var self = this
 
-         axios.get("/api/user/me").then((respose)=>{
-             self.user = respose.data
-         }).catch((err)=>{
-             console.log(err)
-         })
+            axios.get("/api/user/me").then((respose) => {
+                self.user = respose.data
+            }).catch((err) => {
+                console.log(err)
+            })
 
-         console.log(this.user.profile_image)
-     },
-     get_comment_id:function (id) {
-       return "id_comment_"+id
-     },
-     make_comment: function (id, post) {
-         var comment_id = "id_comment_"+id
-         comment_text = document.getElementById(comment_id).value
+            console.log(this.user.profile_image)
+        },
+        get_comment_id: function (id) {
+            return "id_comment_" + id
+        },
+        make_comment: function (id, post) {
+            var comment_id = "id_comment_" + id
+            comment_text = document.getElementById(comment_id).value
 
-         comment = {
+            comment = {
                 "author": this.user,
                 "text": comment_text,
                 "post": id
             }
 
-         console.log(comment)
+            console.log(comment)
 
-         post.comments.push(comment)
+            post.comments.push(comment)
 
-         document.getElementById(comment_id).value = ""
+            document.getElementById(comment_id).value = ""
 
 
+        },
+        infiniteHandler($state) {
+            axios.get("/api/cibling-web/posts", {
+                params: {
+                    page: this.page,
+                },
+            }).then(({data}) => {
+                console.log(data)
+                if (data.results.length) {
+                    this.page += 1;
+                    this.posts.push(...data.results);
+                    $state.loaded();
+                } else {
+                    $state.complete();
+                }
+            });
+        },
 
-     }
-
-   }
+    }
 });
