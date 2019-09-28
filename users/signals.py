0979028ac4641b98a6bb2ffcac28ab22bb2eda94
Ruhshan <ruhshan.ahmed@gmail.com>
2019-09-28 20:32:41 +0600

    Temporary committing signals

--- users/signals.py
+++ users/signals.py
@@ -16,26 +16,26 @@ def save_profile(sender, instance, **kwargs):
     instance.profile.save()
 '''
 
-@receiver(post_save, sender=Profile)
-def create_profile_info(sender, instance, created, **kwargs):
-    if created:
-        ProfileInfo.objects.create(profile=instance)
-
-@receiver(post_save, sender=Profile)
-def save_profile(sender, instance, **kwargs):
-    #print(instance)
-    instance.profileinfo.save()
-
-
-@receiver(pre_save, sender=User)
-def check_email(sender,instance,**kwargs):
-    try:
-        usr = User.objects.get(email=instance.email)
-        # If email exists but it is assigned with the same username than we are overlooking here (above)
-        # This (below) check is redundant but I am still keeping it
-        if usr.username == instance.username:
-            pass
-        else:
-            raise Exception('Email is already registered.')
-    except User.DoesNotExist:
-        pass
+# @receiver(post_save, sender=Profile)
+# def create_profile_info(sender, instance, created, **kwargs):
+#     if created:
+#         ProfileInfo.objects.create(profile=instance)
+#
+# @receiver(post_save, sender=Profile)
+# def save_profile(sender, instance, **kwargs):
+#     #print(instance)
+#     instance.profileinfo.save()
+#
+#
+# @receiver(pre_save, sender=User)
+# def check_email(sender,instance,**kwargs):
+#     try:
+#         usr = User.objects.get(email=instance.email)
+#         # If email exists but it is assigned with the same username than we are overlooking here (above)
+#         # This (below) check is redundant but I am still keeping it
+#         if usr.username == instance.username:
+#             pass
+#         else:
+#             raise Exception('Email is already registered.')
+#     except User.DoesNotExist:
+#         pass
