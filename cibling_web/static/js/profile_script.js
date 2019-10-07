caa8a5f00c5ddc686d6431be5baff1c62d712176
Ruhshan <ruhshan.ahmed@gmail.com>
2019-10-07 23:13:30 +0600

    Enabled updating cropped profile image

--- cibling_web/static/js/profile_script.js
+++ cibling_web/static/js/profile_script.js
@@ -2,42 +2,44 @@ var profileImageApp = new Vue({
     el: "#profileApp",
     data: {
         imageData: "",
-        coverimageData:"",
-        imageUploadProgress : 0,
-        profileImageUploadProgress : 0,
+        coverimageData: "",
+        imageUploadProgress: 0,
+        profileImageUploadProgress: 0,
         imageUpdated: false
     },
     created() {
     },
     methods: {
         showModal: function (name) {
-            if(name === 'profile'){
+            if (name === 'profile') {
                 $("#profileImageChangeModal").modal('show');
             }
-            if(name === 'cover'){
+            if (name === 'cover') {
                 $("#coverImageChangeModal").modal('show');
             }
 
 
         },
-        closeModal: function(name){
-            if(name === 'profile'){
+        closeModal: function (name) {
+            if (name === 'profile') {
                 $("#profileImageChangeModal").modal('hide');
             }
-            if(name === 'cover'){
+            if (name === 'cover') {
                 $("#coverImageChangeModal").modal('hide');
             }
 
 
-            if(this.imageUpdated){
+            if (this.imageUpdated) {
                 window.location.reload();
             }
 
         },
         previewImage: function (event) {
+            $("#id_profile_preview").cropper('destroy');
             this.imageUploadProgress = 0;
             this.imageUpdated = false;
             var input = event.target;
+
 
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
@@ -45,11 +47,12 @@ var profileImageApp = new Vue({
                     this.imageData = e.target.result;
                 }
                 reader.readAsDataURL(input.files[0]);
+
             }
         },
-        previewImageCover: function(event){
-          var input = event.target;
-          if (input.files && input.files[0]) {
+        previewImageCover: function (event) {
+            var input = event.target;
+            if (input.files && input.files[0]) {
                 var reader = new FileReader();
                 reader.onload = (e) => {
                     this.coverimageData = e.target.result;
@@ -58,40 +61,41 @@ var profileImageApp = new Vue({
             }
         },
         uploadProfileImg: function () {
-            var input = document.getElementById("profileImageInput");
+
+            var blob = this.b64ToBlob("#id_profile_preview");
 
             var formData = new FormData();
-            formData.append("image", input.files[0]);
 
-            var self=this;
+            formData.append("image", blob,this.getProfileImageName());
 
             axios.put("/api/user/update-profile-img/" + document.getElementById("user_pk").value,
                 formData, this.get_axios_config("profileImage")).then((response) => {
                 console.log("Done");
-                this.imageUpdated=true;
-            }).catch((e)=>{
+                this.imageUpdated = true;
+            }).catch((e) => {
                 console.log(e);
             });
+
         },
-        uploadCoverImg: function(){
+        uploadCoverImg: function () {
             var input = document.getElementById("coverImageInput");
             var formData = new FormData();
             formData.append("cover_image", input.files[0]);
             var self = this;
 
             axios.put("/api/user/update-cover-pic/" + document.getElementById("user_pk").value,
-                formData, this.get_axios_config('coverImage')).then((response)=>{
-                    console.log("Done");
-                    this.imageUpdated = true;
-            }).catch((e)=>{
+                formData, this.get_axios_config('coverImage')).then((response) => {
+                console.log("Done");
+                this.imageUpdated = true;
+            }).catch((e) => {
                 console.log(e);
             })
 
 
         },
-        getIdFromPath(){
-            var splitted =  document.URL.split("/");
-            return splitted[splitted.length -2];
+        getIdFromPath() {
+            var splitted = document.URL.split("/");
+            return splitted[splitted.length - 2];
         },
         get_axios_config: function (progressName) {
             var csrftoken = this.getCookie('csrftoken');
@@ -111,11 +115,11 @@ var profileImageApp = new Vue({
 
             }
         },
-        update_progress_bar:function(progressName, value){
-            if(progressName === "profileImage"){
+        update_progress_bar: function (progressName, value) {
+            if (progressName === "profileImage") {
                 this.imageUploadProgress = value
             }
-            if(progressName === "coverImage"){
+            if (progressName === "coverImage") {
                 this.profileImageUploadProgress = value
             }
         },
@@ -132,8 +136,57 @@ var profileImageApp = new Vue({
             }
 
             return cookieValue;
+        },
+        enableCropper: function (id) {
+            $(id).cropper({
+                aspectRatio: 1
+            });
+        },
+        b64ToBlob: function (id) {
+            var imageURL = $(id).cropper('getCroppedCanvas').toDataURL("image/png");
+            var block = imageURL.split(";");
+            // Get the content type of the image
+            var contentType = block[0].split(":")[1];// In this case "image/gif"
+            // get the real base64 content of the file
+            var b64Data = block[1].split(",")[1];// In this case "R0lGODlhPQBEAPeoAJosM...."
+
+
+            contentType = contentType || '';
+            sliceSize = 512;
+
+            var byteCharacters = atob(b64Data);
+            var byteArrays = [];
+
+            for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+                var byteNumbers = new Array(slice.length);
+                for (var i = 0; i < slice.length; i++) {
+                    byteNumbers[i] = slice.charCodeAt(i);
+                }
+
+                var byteArray = new Uint8Array(byteNumbers);
+
+                byteArrays.push(byteArray);
+            }
+
+            var blob = new Blob(byteArrays, {type: contentType});
+            return blob;
+        },
+        getProfileImageName(){
+            var name = "profile_"+document.getElementById("user_pk").value+".png";
+            return name;
         }
 
+    },
+    watch: {
+        imageData: function (val) {
+            // this.enableCropper("#id_profile_preview");
+            var self = this;
+            setTimeout(function () {
+                self.enableCropper("#id_profile_preview");
+            }, 1000);
+        }
     }
 
 });
