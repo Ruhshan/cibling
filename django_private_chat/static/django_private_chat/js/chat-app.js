8d1dd06a4785a5ea0798a1a84725baae5eddd342
Ruhshan <ruhshan.ahmed@gmail.com>
2019-12-15 23:15:33 +0600

    Auto render message on recieve

--- django_private_chat/static/django_private_chat/js/chat-app.js
+++ django_private_chat/static/django_private_chat/js/chat-app.js
@@ -1,10 +1,14 @@
 
 var app = new Vue({
     el: "#chat-app",
+    data:{
+      messages:[]
+    },
     created(){
         this.websocket = new WebSocket('ws://localhost:5002/'+this.getRequestSessionId()+'/'+this.getOpponentUserName());
 
         this.websocket.onopen = this.socketOnOpen;
+        this.websocket.onmessage = this.socketOnMessage;
 
     },
     methods:{
@@ -16,6 +20,25 @@ var app = new Vue({
         },
         socketOnOpen:(event)=>{
             console.log("connection success!!!")
+        },
+        addNewMessage:function (packet) {
+          this.messages.push(packet);
+        },
+        socketOnMessage:function(event){
+
+            var packet;
+
+            try {
+                packet = JSON.parse(event.data);
+            } catch (e) {
+                console.log(e);
+            }
+
+            switch (packet.type) {
+                case "new-message":
+                    this.addNewMessage(packet);
+                    break;
+            }
         }
     }
 });
