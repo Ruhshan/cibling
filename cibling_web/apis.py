185e5677a9962c2b725255b17dbd18d3a314144f
Ruhshan <ruhshan.ahmed@gmail.com>
2020-02-06 23:35:21 +0600

    Multiple photos can be uploaded as base64

--- cibling_web/apis.py
+++ cibling_web/apis.py
@@ -52,3 +52,59 @@ class CommentView(APIView):
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
+class CreatePostView(APIView):
+    def post(self, request):
+        print("Post received")
+        print(request.data['images'][0]['val'])
+
+        im = decode_base64_file(request.data['images'][0]['val'])
+
+        print(im)
+        return Response("ok", status=status.HTTP_201_CREATED)
+
+
+
+def decode_base64_file(data):
+
+    def get_file_extension(file_name, decoded_file):
+        import imghdr
+
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
+
+    from django.core.files.base import ContentFile
+    import base64
+    import six
+    import uuid
+
+    # Check if this is a base64 string
+    if isinstance(data, six.string_types):
+        # Check if the base64 string is in the "data:" format
+        if 'data:' in data and ';base64,' in data:
+            # Break out the header from the base64 content
+            header, data = data.split(';base64,')
+
+        # Try to decode the file. Return validation error if it fails.
+        try:
+            decoded_file = base64.b64decode(data)
+        except TypeError:
+            TypeError('invalid_image')
+
+        # Generate file name:
+        file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
+        # Get the file name extension:
+        file_extension = get_file_extension(file_name, decoded_file)
+
+        complete_file_name = "%s.%s" % (file_name, file_extension, )
+
+        return ContentFile(decoded_file, name=complete_file_name)
+
+
+#
+# import decode_base64_file
+#
+# p = Post(content='My Picture', image=decode_based64_file(your_base64_file))
+# p.save()
